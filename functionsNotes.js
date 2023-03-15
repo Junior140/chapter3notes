f1044ec0426f01b7c8ad63a57fe95f1f77ef0d16
6svu <6svu@fresnou.org>
2023-03-15 10:16:21 -0700

    intial commit

---
+++ functionsNotes.js
@@ -0,0 +1,35 @@
+//Functions Notes
+//Step 1: Define the function
+function sayHello(){
+    console.log('Hello');
+}
+// Step 2;Call (or use) the function
+sayHello();
+
+function printmax(){
+    let x = 6;
+    let y = 29;
+    if (x > y){
+        console.log(`The larger number is ${x}.`);
+    } else {
+        console.log(`The larger number is ${y}.`);
+    }
+}
+
+printMax();{
+    if (x > y){
+        console.log(`The larger number is ${x}.`);
+    } else {
+        console.log(`The larger number is ${y}.`);
+    }
+}
+
+printMax(16,9);
+printMax(2,75);
+printMax(2, 2);
+
+function sum(num1, num2){
+    console.log(`${num1}+ ${num2} = ${num1 + num2}`);
+}
+
+sum()
